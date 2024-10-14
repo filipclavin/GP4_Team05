6c62e49cc2bfa5887f32ebb57bf66cb161680e88
filipclavin <filipclavin99@gmail.com>
2024-10-14 15:11:57 +0200

    fix: forgot to implement overriding depth scaling factor

--- Source/GP4_Team05/EnemySpawner.h
+++ Source/GP4_Team05/EnemySpawner.h
@@ -51,7 +51,7 @@ class GP4_TEAM05_API AEnemySpawner : public AActor
 	 * where room depth starts at 0.
 	 *
 	 * May be overridden by individual enemy groups. */
-	UPROPERTY(EditAnywhere) float DepthScalingFactor = 0.0f;
+	UPROPERTY(EditAnywhere) float _depthScalingFactor = 0.0f;
 	
 	int32			_currentWaveIndex = -1;
 	FTimerHandle	_waveTimer;
@@ -74,7 +74,7 @@ class GP4_TEAM05_API AEnemySpawner : public AActor
 	void DespawnEnemy(ABaseEnemyClass* enemy);
 	void PrepareEnemy(FEnemyGroup& group);
 	void PrepareEnemies();
-	int ApplyRoomDepthMultiplier(int count) const;
+	int ApplyRoomDepthMultiplier(int count, float depthScalingFactor ) const;
 	
 public:	
 	// Sets default values for this actor's properties
