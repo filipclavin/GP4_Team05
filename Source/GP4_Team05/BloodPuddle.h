d728f3e6e0778c4f2327d725e4fc61f5ca6a7666
Ana-MarijaPiperkoska <piperkoskaanamarija@gmail.com>
2024-10-16 15:51:00 +0200

    Fixed revert commit files
    A lot of files were edited during revert, fixed so we kept peoples progress.

--- Source/GP4_Team05/BloodPuddle.h
+++ Source/GP4_Team05/BloodPuddle.h
@@ -71,7 +71,7 @@ protected:
 public:	
 	
 	virtual void Tick(float DeltaTime) override;
-	UFUNCTION(BlueprintCallable, Category = "BloodPuddle")
+	//UFUNCTION(BlueprintCallable, Category = "BloodPuddle")
 	//static ABloodPuddle* SpawnPuddle(FVector SpawnLocation, FRotator SpawnRotation);
 
 };
